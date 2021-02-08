c76bca52273d28edb63d54733a599d628c6880f9
Vova Rodin <mynameisvova@yahoo.com>
2021-02-08 11:03:56 +0300

    footer

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -8,12 +8,66 @@ import Container from '@components/layout/Container/Container'
 export default function Footer () {
   return (
     <footer className={scss.footer}>
-      <Container>
+      <Container className={scss.container}>
         <Link href="https://вязаный.рф">
           <a className={scss.logo} target="_blank">
             <ReactSVG src="svg/logo-brand.svg" />
           </a>
         </Link>
+        <nav className={scss.nav}>
+          <span className={scss.nav_item}>
+            <Link href="#">
+              <a>Вязаный.рф интернет-магазин пряжи</a>
+            </Link>
+          </span>
+          <span className={scss.nav_item}>
+            <Link href="#">
+              <a>Мастер-классы</a>
+            </Link>
+          </span>
+          <span className={scss.nav_item}>
+            <Link href="#">
+              <a>Доставка по России от 1 мотка</a>
+            </Link>
+          </span>
+          <span className={scss.nav_item}>
+            <Link href="#">
+              <a>Конкурс</a>
+            </Link>
+          </span>
+          <span className={scss.nav_item}>
+            <Link href="#">
+              <a>Каталог</a>
+            </Link>
+          </span>
+          <span className={scss.nav_item}>
+            <Link href="#">
+              <a>Скидки</a>
+            </Link>
+          </span>
+        </nav>
+        <nav className={scss.sm}>
+          <div className={scss.sm_caption}>
+            Вязаный в соцсетях
+          </div>
+          <div className={scss.sm_list}>
+            <Link href="https://www.instagram.com/vazanyi.rf/">
+              <a target="_blank">
+                <ReactSVG src="svg/sm/in.svg" />
+              </a>
+            </Link>
+            <Link href="https://vk.com/vazanyirf">
+              <a target="_blank">
+                <ReactSVG src="svg/sm/vk.svg" />
+              </a>
+            </Link>
+            <Link href="https://www.youtube.com/channel/UCIRRlU4uaSHAnVTKFslBFhQ">
+              <a target="_blank">
+                <ReactSVG src="svg/sm/yt.svg" />
+              </a>
+            </Link>
+          </div> 
+        </nav>
       </Container>
     </footer>
   )
