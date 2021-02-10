0f9998703e2e8a8badb57feb695ca4bae5d19226
Vova Rodin <mynameisvova@yahoo.com>
2021-02-10 14:51:57 +0300

    move getData to fixtures

--- src/components/constructor/LoopPanel/LoopPanel.js
+++ src/components/constructor/LoopPanel/LoopPanel.js
@@ -5,33 +5,7 @@ import classNames from 'classnames'
 import LoopButton from '@components/constructor/LoopButton/LoopButton'
 
 // TODO: replace this shit with api 
-const getData = () => {
-  const data = []
-
-  for (let j = 1; j <= 60; j++) {
-    if (j != 13 && j != 14 && j != 15 &&
-        j != 16 && j != 28 && j != 29 &&
-        j != 30 && j != 31 && j != 32) {
-      data.push({
-        id: j,
-      })
-    }
-  }
-
-  data[0].hint = 'пустая петля'
-  data[1].hint = 'лицевая'
-  data[3].hint = '2 петли провязать <br/> вместе изнаночной'
-
-  data[11].options = [{ id: 13 }, { id: 14 }, { id: 15 }, { id: 16 }]
-  data[22].options = [
-    {
-      id: 28,
-      hint: 'из 1 петли вывязать 2: <br> сначала провязать 1 петлю лицевой <br> и, не снимая ее с левой спицы, <br> провязать еще раз лицевой скрещенной'
-    }, { id: 29 }, { id: 30 }, { id: 31 }, { id: 32 }
-  ]
-  
-  return data
-}
+import { getData } from '@src/fixtures/loops'
 
 export default function LoopsBar ({ className }) {
   return (
