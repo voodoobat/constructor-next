e14e6fe7aefc3e63dc086af905dd2fe3c563e7cb
Vova Rodin <mynameisvova@yahoo.com>
2021-02-10 13:38:08 +0300

    loops hints

--- src/components/constructor/LoopButton/LoopButton.js
+++ src/components/constructor/LoopButton/LoopButton.js
@@ -12,14 +12,28 @@ export default function LoopButton ({ className, data }) {
     <div className={classNames(className, scss._, selected ? scss.is_selected : '')}>
       <div className={classNames(scss.button, scss.button_main)}>
         <Loop id={data.id} />
+        {data.hint &&
+          <div className={classNames(scss.hint, scss.hint_down)}
+               dangerouslySetInnerHTML={{
+                 __html: data.hint
+               }}>
+          </div>
+        }
         {data.options && <OptionsIcon className={scss.options_icon} />}
       </div>
       {data.options && (
         <div className={scss.dropdown}>
           <div className={scss.dropdown_data}>
-            {data.options.map(({ id }) => (
+            {data.options.map(({ id, hint }) => (
               <div className={scss.button} key={id}>
                 <Loop id={id} />
+                {hint &&
+                  <div className={classNames(scss.hint, scss.hint_right)}
+                      dangerouslySetInnerHTML={{
+                        __html: hint
+                      }}>
+                  </div>
+                }
               </div>
             ))}
           </div>
