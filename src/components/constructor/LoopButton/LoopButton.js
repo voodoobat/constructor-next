64688549637f61506e40e65aeec4f8e4e67a9f75
Vova Rodin <mynameisvova@yahoo.com>
2021-02-10 12:34:38 +0300

    loop button dropdown

--- src/components/constructor/LoopButton/LoopButton.js
+++ src/components/constructor/LoopButton/LoopButton.js
@@ -3,11 +3,26 @@ import scss from './LoopButton.module.scss'
 import classNames from 'classnames'
 
 import Loop from '@components/constructor/Loop/Loop'
+import OptionsIcon from './svg/options_icon.svg'
 
-export default function LoopButton ({ className, id }) {
+export default function LoopButton ({ className, data }) {
   return (
     <div className={classNames(className, scss._)}>
-      <Loop id={id} />
+      <div className={classNames(scss.button, scss.button_is_selected)}>
+        <Loop id={data.id} />
+        {data.options && <OptionsIcon className={scss.options_icon} />}
+      </div>
+      {data.options && (
+        <div className={scss.dropdown}>
+          <div className={scss.dropdown_data}>
+            {data.options.map(({ id }) => (
+              <div className={scss.button} key={id}>
+                <Loop id={id} />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/components/constructor/LoopPanel/LoopPanel.js
+++ src/components/constructor/LoopPanel/LoopPanel.js
@@ -5,18 +5,33 @@ import classNames from 'classnames'
 import LoopButton from '@components/constructor/LoopButton/LoopButton'
 
 // TODO: replace this shit with api 
-const loops = []
-for (let j = 1; j <= 60; j++) {
-  loops.push(j)
+const getData = () => {
+  const data = []
+
+  for (let j = 1; j <= 60; j++) {
+    if (j != 13 && j != 14 &&
+        j != 15 && j != 16 &&
+        j != 29 && j != 30 &&
+        j != 31 && j != 32) {
+
+      data.push({ id: j })
+    }
+  }
+
+  data[11].options = [{ id: 13 }, { id: 14 }, { id: 15 }, { id: 16 }]
+  data[23].options = [{ id: 29 }, { id: 30 }, { id: 31 }, { id: 32 }]
+
+
+  return data
 }
 
 export default function LoopsBar ({ className }) {
   return (
     <div className={classNames(className, scss._)}>
-      {loops.map(id =>
+      {getData().map(loop =>
         <LoopButton className={scss.button}
-                    id={id}
-                    key={id} />
+                    data={loop}
+                    key={loop.id} />
       )}
     </div>
   )
