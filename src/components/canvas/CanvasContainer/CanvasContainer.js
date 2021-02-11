ed9dec553bdc94bd704060a78af088ac4394ffa0
Vova Rodin <mynameisvova@yahoo.com>
2021-02-11 11:06:29 +0300

    zoom control

--- src/components/canvas/CanvasContainer/CanvasContainer.js
+++ src/components/canvas/CanvasContainer/CanvasContainer.js
@@ -3,8 +3,10 @@ import scss from './CanvasContainer.module.scss'
 import classNames from 'classnames'
 
 import Canvas from '@components/canvas/Canvas/Canvas'
+import Zoom from '@components/canvas/Zoom/Zoom'
 
 export default function CanvasContainer ({ className }) {
+
   return (
     <div className={classNames(className, scss._)}>
       <Canvas matrix={[
@@ -18,6 +20,9 @@ export default function CanvasContainer ({ className }) {
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
       ]} />
+      <div className={classNames(scss.bottom)}>
+        <Zoom />
+      </div>
     </div>
   )
 }
--- src/components/canvas/Zoom/Zoom.js
+++ src/components/canvas/Zoom/Zoom.js
@@ -1,11 +1,26 @@
-import scss from './Empty.module.scss'
+import scss from './Zoom.module.scss'
 
 import classNames from 'classnames'
 
-export default function Empty ({ className, children }) {
+import IncIcon from './svg/inc.svg'
+import DecIcon from './svg/dec.svg'
+
+export default function Zoom ({ className }) {
   return (
     <div className={classNames(className, scss._)}>
-      {children}
+      <button
+        className={scss.button}
+        type="button">
+        <DecIcon />
+      </button>
+      <span className={scss.value}>
+        100%
+      </span>
+      <button
+        className={scss.button}
+        type="button">
+        <IncIcon />
+      </button>
     </div>
   )
 }
