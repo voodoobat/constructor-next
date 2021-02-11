9c688a541ffd112ab414d8b54f5b678f90498ea7
Vova Rodin <mynameisvova@yahoo.com>
2021-02-11 10:18:39 +0300

    eye: replace active -> inactive

--- src/components/canvas/Canvas/Canvas.js
+++ src/components/canvas/Canvas/Canvas.js
@@ -5,7 +5,6 @@ import classNames from 'classnames'
 export default function Canvas ({ className }) {
   return (
     <div className={classNames(className, scss._)}>
-      {/*  */}
     </div>
   )
 }
