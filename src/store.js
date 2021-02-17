432ad7361361f91c443b753c289262da9b0ca8db
Vova Rodin <mynameisvova@yahoo.com>
2021-02-17 09:43:34 +0300

    dev store

--- src/store.js
+++ src/store.js
@@ -3,13 +3,18 @@ import thunk from 'redux-thunk'
 
 import reducer from '@src/reducers'
 
-const store = typeof window == 'undefined'
-  ? createStore(reducer, applyMiddleware(thunk))
-  : createStore(reducer, compose(
-      applyMiddleware(thunk),
-      // process.env.NODE_ENV == 'development' &&
-      // window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      // window.__REDUX_DEVTOOLS_EXTENSION__()
-    ))
+const isDev = (
+  typeof window != 'undefined' &&
+  typeof window.__REDUX_DEVTOOLS_EXTENSION__ != 'undefined'
+)
+
+let store = createStore(reducer, applyMiddleware(thunk))
+
+if (isDev) {
+  store = createStore(reducer, compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+  ))
+}
 
 export default store
