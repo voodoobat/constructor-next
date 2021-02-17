d696f850642b61172611c9ebc41f82d729810f12
Vova Rodin <mynameisvova@yahoo.com>
2021-02-17 15:45:52 +0300

    group masonry layout

--- src/components/canvas/Group/Group.js
+++ src/components/canvas/Group/Group.js
@@ -6,9 +6,15 @@ import Row from '@components/canvas/Row/Row'
 import Cell from '@components/canvas/Cell/Cell'
 import Loop from '@components/constructor/Loop/Loop'
 
+import Delete from './svg/delete.svg'
+
 export default function Group ({ className, group }) {
   return (
     <div className={classNames(className, scss._)}>
+      <button className={scss.delete}
+              type="button">
+        <Delete />
+      </button>
       <div className={scss.canvas}>
         {group.canvas.map((element, y) => (
           <Row key={y}>
--- src/components/canvas/GroupContainer/GroupContainer.js
+++ src/components/canvas/GroupContainer/GroupContainer.js
@@ -1,15 +1,16 @@
 import scss from './GroupContainer.module.scss'
 
 import { connect } from 'react-redux'
+import Masonry from 'react-masonry-component'
 import classNames from 'classnames'
 
 import Group from '@components/canvas/Group/Group'
 
 function GroupContainer ({ className, groups }) {
   return (
-    <div className={classNames(className, scss._)}>
+    <Masonry className={classNames(className, scss._)}>
       {groups.map((group, key) => <Group group={group} key={key} />)}
-    </div>
+    </Masonry>
   )
 }
 
