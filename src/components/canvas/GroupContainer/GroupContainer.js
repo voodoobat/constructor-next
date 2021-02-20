d5ccc5768209551de58aa1bf971bdfcd8a71a0b7
Vova Rodin <mynameisvova@yahoo.com>
2021-02-20 11:27:53 +0300

    group dropdown

--- src/components/canvas/GroupContainer/GroupContainer.js
+++ src/components/canvas/GroupContainer/GroupContainer.js
@@ -5,15 +5,20 @@ import Masonry from 'react-masonry-component'
 import classNames from 'classnames'
 
 import Group from '@components/canvas/Group/Group'
+import Dropdown from '@components/common/Dropdown/Dropdown'
 
 function GroupContainer ({ className, groups }) {
   return (
-    <Masonry className={classNames(className, scss._)}>
-      {groups.map((group, key) =>
-        <Group group={group}
-               key={key} />
-      )}
-    </Masonry>
+    <Dropdown className={classNames(className, scss._)}
+              caption="Мои группы элементов"
+              size="sm">
+      <Masonry className={scss.content}>
+        {groups.map((group, key) =>
+          <Group group={group}
+                key={key} />
+        )}
+      </Masonry>
+    </Dropdown>
   )
 }
 
