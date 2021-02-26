6eb36978d8c7f79476e822a8b6e317ddc7468579
Vova Rodin <mynameisvova@yahoo.com>
2021-02-26 11:10:08 +0300

    groups placeholder

--- src/components/canvas/GroupContainer/GroupContainer.js
+++ src/components/canvas/GroupContainer/GroupContainer.js
@@ -8,7 +8,11 @@ import Group from '@components/canvas/Group/Group'
 import Plait from '@components/canvas/Plait/Plait'
 import Overlay from '@components/common/Overlay/Overlay'
 
+import QuestionIcon from './svg/question.svg'
+import Hint from '@components/common/Hint/Hint'
+
 function GroupContainer ({ className, groups, plaits, inactive }) {
+
   return (
     <div className={classNames(className, scss._)}>
       <Dropdown caption="Элементы для кос"
@@ -22,18 +26,31 @@ function GroupContainer ({ className, groups, plaits, inactive }) {
           )}
         </div>
       </Dropdown>
-      {Boolean(groups.length) &&
+      {groups.length ? (
         <Dropdown caption="Мои группы элементов"
                   size="sm">
           <div className={scss.content}>
             {groups.map((group, key) =>
               <Group className={scss.group}
-                     group={group}
-                     key={key} />
+                    group={group}
+                    key={key} />
             )}
           </div>
         </Dropdown>
-      }
+      ) : (
+        <div className={scss.no_group}>
+          <span className={scss.no_group_text}>
+            Мои группы элементов
+          </span>
+          <div className={scss.question}>
+            <QuestionIcon />
+            <Hint className={scss.question_hint}
+                  caption="Создай группу">
+              c группами веселей
+            </Hint>
+          </div>
+        </div>
+      )}
 
       {inactive && <Overlay />}
     </div>
