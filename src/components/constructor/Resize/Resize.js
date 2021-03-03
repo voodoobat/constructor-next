7f1930660d2a7b7e6ade0289b5ed3008d03d88ce
Vova Rodin <mynameisvova@yahoo.com>
2021-03-03 12:12:07 +0300

    replace resize buttos

--- src/components/constructor/Resize/Resize.js
+++ src/components/constructor/Resize/Resize.js
@@ -21,6 +21,26 @@ function Resize ({ className, dispatch, canvas }) {
         Удалить строку сверху
       </button>
       <button className={scss.button}
+              onClick={() => dispatch(store.commitCanvas(fn.addCellLeft(canvas)))}
+              type="button">
+        Добавить ряд слева
+      </button>
+      <button className={scss.button}
+              onClick={() => dispatch(store.commitCanvas(fn.addCellRight(canvas)))}
+              type="button">
+        Добавить ряд справа
+      </button>
+      <button className={scss.button}
+              onClick={() => dispatch(store.commitCanvas(fn.removeCellLeft(canvas)))}
+              type="button">
+        Удалить ряд слева
+      </button>
+      <button className={scss.button}
+              onClick={() => dispatch(store.commitCanvas(fn.removeCellRight(canvas)))}
+              type="button">
+        Удалить ряд справа
+      </button>
+      <button className={scss.button}
               onClick={() => dispatch(store.commitCanvas(fn.addRowBottom(canvas)))}
               type="button">
         Добавить строку снизу
@@ -29,26 +49,6 @@ function Resize ({ className, dispatch, canvas }) {
               onClick={() => dispatch(store.commitCanvas(fn.removeRowBottom(canvas)))}
               type="button">
         Удалить строку снизу
-      </button>
-      <button className={scss.button}
-              onClick={() => dispatch(store.commitCanvas(fn.addCellRight(canvas)))}
-              type="button">
-        Добавить ряд справа
-      </button>
-      <button className={scss.button}
-              onClick={() => dispatch(store.commitCanvas(fn.removeCellRight(canvas)))}
-              type="button">
-        Удалить ряд справа
-      </button>
-      <button className={scss.button}
-              onClick={() => dispatch(store.commitCanvas(fn.addCellLeft(canvas)))}
-              type="button">
-        Добавить ряд слева
-      </button>
-      <button className={scss.button}
-              onClick={() => dispatch(store.commitCanvas(fn.removeCellLeft(canvas)))}
-              type="button">
-        Удалить ряд слева
       </button>
     </div>
   )
